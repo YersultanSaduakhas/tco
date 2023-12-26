b2b2b82265ad13bf57b2cc290d97ab13febe82ba
YersultanSaduakhas <zver-yersul@mail.ru>
2023-12-26 15:07:55 +0600

    new custom chart widget

---
+++ chart-widget/src/config.ts
@@ -0,0 +1,11 @@
+import { type ImmutableObject } from 'seamless-immutable'
+
+export interface Config {
+  ///query?&text=&objectIds=&time=&timeRelation=esriTimeRelationOverlaps&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Foot&relationParam=&outFields=year_%2Cmo&returnGeometry=false&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&havingClause=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&historicMoment=&returnDistinctValues=false&resultOffset=&resultRecordCount=&returnExtentOnly=false&sqlFormat=none&datumTransformation=&parameterValues=&rangeValues=&quantizationParameters=&featureEncoding=esriDefault&f=pjson
+  title: string,
+  layerServiceUrl:string;
+  xAxis:string;
+  yAxisess:any[];
+}
+
+export type IMConfig = ImmutableObject<Config>
